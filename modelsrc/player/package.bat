e562abc756d748235f458de4a43b0398180f2145
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-01-09 16:17:28 -0500

    Added missing copy of MDL texture file.

--- modelsrc/player/package.bat
+++ modelsrc/player/package.bat
@@ -8,5 +8,6 @@ set basename=player
 rem Copy Gordon player model
 
 if exist %basename%_gordon.mdl xcopy /F /Y %basename%_gordon.mdl %~dp0..\..\models\%basename%.mdl*
+if exist %basename%_gordonT.mdl xcopy /F /Y %basename%_gordonT.mdl %~dp0..\..\models\%basename%T.mdl*
 
 endlocal
