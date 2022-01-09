0f4a5cff99b3fc5f88aa4f84f6472c7c2ad68a59
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-01-09 16:20:42 -0500

    Added Blue Shift player model.

--- modelsrc/player/package.bat
+++ modelsrc/player/package.bat
@@ -5,6 +5,11 @@ cls
 
 set basename=player
 
+rem Copy Barney player model
+
+if exist %basename%_barney.mdl xcopy /F /Y %basename%_barney.mdl %~dp0..\..\models\bshift\%basename%.mdl*
+if exist %basename%_barneyT.mdl xcopy /F /Y %basename%_barneyT.mdl %~dp0..\..\models\bshift\%basename%T.mdl*
+
 rem Copy Gordon player model
 
 if exist %basename%_gordon.mdl xcopy /F /Y %basename%_gordon.mdl %~dp0..\..\models\%basename%.mdl*
