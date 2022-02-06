f88a2a9e191b1d2c58dd93038aa9552e2836f8f6
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-02-06 11:25:10 -0500

    Added LD Op4 scientist model.

--- modelsrc/scientist/package.bat
+++ modelsrc/scientist/package.bat
@@ -6,5 +6,6 @@ cls
 set basename=scientist
 
 if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
+if exist %basename%_ld_op4.mdl xcopy /F /Y %basename%_ld_op4.mdl %~dp0..\..\models\op4\%basename%.mdl*
 
 endlocal
