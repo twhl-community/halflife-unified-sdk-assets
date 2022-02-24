07daafabb9c6051e763d5a7679005594acdf2c56
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-02-24 18:45:49 -0500

    Merged Op4 scientist into HL1.

--- modelsrc/scientist/package.bat
+++ modelsrc/scientist/package.bat
@@ -6,11 +6,9 @@ cls
 set basename=scientist
 
 if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
-if exist %basename%_ld_op4.mdl xcopy /F /Y %basename%_ld_op4.mdl %~dp0..\..\models\op4\%basename%.mdl*
 if exist %basename%_ld_bshift.mdl xcopy /F /Y %basename%_ld_bshift.mdl %~dp0..\..\models\bshift\%basename%.mdl*
 
 if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
-if exist %basename%_hd_op4.mdl xcopy /F /Y %basename%_hd_op4.mdl %~dp0..\..\models_hd\op4\%basename%.mdl*
 if exist %basename%_hd_bshift.mdl xcopy /F /Y %basename%_hd_bshift.mdl %~dp0..\..\models_hd\bshift\%basename%.mdl*
 
 endlocal
