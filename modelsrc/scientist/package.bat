c04de4845f8a19f679cf38c72fdc150875863cb2
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-02-26 18:03:12 -0500

    Merged Blue Shift HD scientist into HL1 HD scientist.

--- modelsrc/scientist/package.bat
+++ modelsrc/scientist/package.bat
@@ -9,6 +9,5 @@ if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%base
 if exist %basename%_ld_bshift.mdl xcopy /F /Y %basename%_ld_bshift.mdl %~dp0..\..\models\bshift\%basename%.mdl*
 
 if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
-if exist %basename%_hd_bshift.mdl xcopy /F /Y %basename%_hd_bshift.mdl %~dp0..\..\models_hd\bshift\%basename%.mdl*
 
 endlocal
