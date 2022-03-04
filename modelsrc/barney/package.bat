3ae7b97ab993ebd8267ba314a5f35e51f53715a9
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-03-04 17:43:52 -0500

    Merged Gearbox HD barney into HL1 HD barney.

--- modelsrc/barney/package.bat
+++ modelsrc/barney/package.bat
@@ -7,9 +7,5 @@ set basename=barney
 
 if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
 if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
-if exist %basename%_hd_gearbox.mdl (
-  xcopy /F /Y %basename%_hd_gearbox.mdl %~dp0..\..\models_hd\op4\%basename%.mdl*
-  xcopy /F /Y %basename%_hd_gearbox.mdl %~dp0..\..\models_hd\bshift\%basename%.mdl*
-)
 
 endlocal
