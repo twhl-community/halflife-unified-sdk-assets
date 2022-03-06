b2d7d92351709ba9ca2fa409b12f2feb09880c06
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-03-05 19:55:32 -0500

    Added LD HL1 Rosenberg model.

--- modelsrc/rosenberg/package.bat
+++ modelsrc/rosenberg/package.bat
@@ -5,6 +5,7 @@ cls
 
 set basename=rosenberg
 
+if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
 if exist %basename%_ld_bshift.mdl xcopy /F /Y %basename%_ld_bshift.mdl %~dp0..\..\models\bshift\%basename%.mdl*
 
 endlocal
