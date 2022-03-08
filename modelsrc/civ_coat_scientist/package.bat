cde3a8e49cc85c934a8f5651f981f588f80ee8dc
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-03-08 18:26:32 -0500

    Added LD/HD Civilian scientist models.

--- modelsrc/civ_coat_scientist/package.bat
+++ modelsrc/civ_coat_scientist/package.bat
@@ -3,16 +3,10 @@ setlocal enableextensions enabledelayedexpansion
 prompt $
 cls
 
-set basename=scientist
+set basename=civ_coat_scientist
 
 if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
 if exist %basename%_ld_bshift.mdl xcopy /F /Y %basename%_ld_bshift.mdl %~dp0..\..\models\bshift\%basename%.mdl*
-
 if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
-
-rem Civilian scientists
-
-if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\civ_%basename%.mdl*
-if exist %basename%_ld_bshift.mdl xcopy /F /Y %basename%_ld_bshift.mdl %~dp0..\..\models\bshift\civ_%basename%.mdl*
 
 endlocal
