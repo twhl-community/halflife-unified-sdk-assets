cf3ba0c0347423ae154feecb943bd9b4baf29fc3
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-03-12 08:50:38 -0500

    Added HD cleansuit scientist model.

--- modelsrc/cleansuit_scientist/package.bat
+++ modelsrc/cleansuit_scientist/package.bat
@@ -7,5 +7,6 @@ set basename=cleansuit_scientist
 
 if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
 if exist %basename%_ld_bshift.mdl xcopy /F /Y %basename%_ld_bshift.mdl %~dp0..\..\models\bshift\%basename%.mdl*
+if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
 
 endlocal
