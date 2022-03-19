2b592e4ed4811c03fe0db0d38485793986d8fe45
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-03-19 16:21:22 -0400

    Added LD Blue Shift Gordon scientist model.

--- modelsrc/gordon_scientist/package.bat
+++ modelsrc/gordon_scientist/package.bat
@@ -6,5 +6,6 @@ cls
 set basename=gordon_scientist
 
 if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
+if exist %basename%_ld_bshift.mdl xcopy /F /Y %basename%_ld_bshift.mdl %~dp0..\..\models\bshift\%basename%.mdl*
 
 endlocal
