99f6c9734cfb633abb6d1fd6d75b797413978d9f
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-03-24 17:52:45 -0400

    Added shared LD islave model.

---
+++ modelsrc/islave/package.bat
@@ -0,0 +1,10 @@
+setlocal enableextensions enabledelayedexpansion
+@echo off
+prompt $
+cls
+
+set basename=islave
+
+if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
+
+endlocal
