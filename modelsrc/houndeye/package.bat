9a8895f45fcdb0505e2d00091cc6d64232841e34
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-03-28 18:52:16 -0400

    Added shared LD dead Houndeye model.

--- modelsrc/houndeye/package.bat
+++ modelsrc/houndeye/package.bat
@@ -8,4 +8,10 @@ set basename=houndeye
 if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
 if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
 
+rem Dead houndeye model
+
+set basename=houndeye_dead
+
+if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
+
 endlocal
