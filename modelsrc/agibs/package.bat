0df1ca0be48fb60880636941e18810473833d5e1
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-05-21 15:36:36 -0400

    Added shared LD agibs model.

--- modelsrc/agibs/package.bat
+++ modelsrc/agibs/package.bat
@@ -5,6 +5,7 @@ cls
 
 set basename=agibs
 
+if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
 if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
 
 endlocal
