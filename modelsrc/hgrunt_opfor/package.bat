ddd8c421f2beb02e12f6fb698bb11f29c26f1b40
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-06-11 12:33:20 -0400

    Added shared LD/HD Op4 hgrunt_opfor models.

---
+++ modelsrc/hgrunt_opfor/package.bat
@@ -0,0 +1,11 @@
+setlocal enableextensions enabledelayedexpansion
+@echo off
+prompt $
+cls
+
+set basename=hgrunt_opfor
+
+if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
+if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
+
+endlocal
