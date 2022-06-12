9291b4485f50bdf8f25c4fd12549f5b9a9980fb9
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-06-12 13:38:03 -0400

    Added shared LD/HD intro_saw models.

--- modelsrc/hgrunt_opfor/package.bat
+++ modelsrc/hgrunt_opfor/package.bat
@@ -22,4 +22,11 @@ set basename=intro_regular
 if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
 if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
 
+rem Copy intro_saw.mdl
+
+set basename=intro_saw
+
+if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
+if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
+
 endlocal
