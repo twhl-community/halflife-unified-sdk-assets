d43c27cd274ddb7c68fbb1ae11fa840fb15b9d83
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-06-12 13:37:56 -0400

    Added shared LD/HD intro_regular models.

--- modelsrc/hgrunt_opfor/package.bat
+++ modelsrc/hgrunt_opfor/package.bat
@@ -15,4 +15,11 @@ set basename=intro_commander
 if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
 if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
 
+rem Copy intro_regular.mdl
+
+set basename=intro_regular
+
+if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
+if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
+
 endlocal
