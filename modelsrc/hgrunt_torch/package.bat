302082399d92bfd89a445acf4cd5f946c01f5b5e
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2022-06-12 13:37:17 -0400

    Added shared LD/HD intro_torch models.

--- modelsrc/hgrunt_torch/package.bat
+++ modelsrc/hgrunt_torch/package.bat
@@ -8,4 +8,11 @@ set basename=hgrunt_torch
 if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
 if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
 
+rem Copy intro_torch.mdl
+
+set basename=intro_torch
+
+if exist %basename%_ld.mdl xcopy /F /Y %basename%_ld.mdl %~dp0..\..\models\%basename%.mdl*
+if exist %basename%_hd.mdl xcopy /F /Y %basename%_hd.mdl %~dp0..\..\models_hd\%basename%.mdl*
+
 endlocal
