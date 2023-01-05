4a48863b3069beee1e7ff5e8997ffe047c7635f4
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2023-01-05 13:18:49 -0500

    Added shared LD holo cart model.

--- modelsrc/models/holo/package.bat
+++ modelsrc/models/holo/package.bat
@@ -6,5 +6,6 @@ cls
 set basename=holo
 
 if exist %basename%.mdl xcopy /F /Y %basename%.mdl %~dp0..\..\mdl\ld\%basename%.mdl*
+if exist %basename%_cart.mdl xcopy /F /Y %basename%_cart.mdl %~dp0..\..\mdl\ld\%basename%_cart.mdl*
 
 endlocal
